9208f7eff801ab0a1f6cc59633a1cc54612688f8
1max87 <48288135+1max87@users.noreply.github.com>
2019-03-31 08:43:35 +0500

    Add files via upload

---
+++ HashTable/HashTable/HashTable/HashTable.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class HashTable
+    {
+        //private Tuple<int, List<object>>[] data;
+        class HashData
+        {
+            public readonly int KeyHash;
+            public  object Value { get; set; }
+
+            public HashData(int hash, object obj)
+            {
+                KeyHash = hash;
+                Value = obj;
+            }
+        }
+        private List<HashData>[] data;
+        /// <summary>
+        /// Конструктор контейнера
+        /// summary>
+        /// size">Размер хэш-таблицы
+        public HashTable(int size)
+        {
+            data = new List<HashData>[size];
+        }
+        ///
+        /// Метод складывающий пару ключ-значение в таблицу
+        ///
+        /// key">
+        /// value">
+
+        public void PutPair(object key, object value)
+        {
+            var keyHashCode = key.GetHashCode();
+            var index = Math.Abs(keyHashCode) % data.Length;
+
+            if(data[index] == null)
+                data[index] = new List<HashData> { new HashData(keyHashCode,  value )};
+            else
+            {
+                var el = data[index].FirstOrDefault(x => x.KeyHash == keyHashCode);
+                if (el != null) el.Value = value;
+                else data[index].Add(new HashData(keyHashCode, value));
+            }
+            
+        }
+        /// <summary>
+        /// Поиск значения по ключу
+        /// summary>
+        /// key">Ключ
+        /// <returns>Значение, null если ключ отсутствует<returns>
+        public object GetValueByKey(object key)
+        {
+            try
+            {
+                var keyHashCode = key.GetHashCode();
+                var keyValue = data[Math.Abs(keyHashCode) % data.Length];
+                return keyValue.Find(x => x.KeyHash == keyHashCode).Value;                
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+    }
+}
